915a850248782ecb9492bde078654cfe5953a465
zlion95 <zudaojun@outlook.com>
2019-10-22 20:48:44 +0800

    Update kick_start 2019_G_q2 to precompute zeros and ones.

--- kick_start/kick_start_2019_G_q2.cpp
+++ kick_start/kick_start_2019_G_q2.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 class Solution {
 public:
+	int *zeros, *ones;
 	int k;
 	int m;
 
@@ -19,23 +20,33 @@ public:
 		return n - 1;
 	}
 
+	void init_elems(vector<int> &a, int size) {
+		zeros = new int[size];
+		ones = new int[size];
+
+		--size;
+		while (size >= 0) {
+			zeros[size] = 0;
+			ones[size] = 0;
+			int cur_k = 1 << size;
+			for (int i = 0; i < a.size(); ++i) {
+				if ((a[i] & cur_k) == 0) zeros[size] += cur_k;
+				else ones[size] += cur_k;
+			}
+			--size;
+		}
+	}
+
 	bool get_value(vector<int> &a, int iofk, int value) {
-		int v1 = value, v2 = value, cur_k;
 		if (iofk < 0) return true;
 
-		cur_k = 1 << iofk;
-		for (int i = 0; i < a.size(); ++i) {
-			if ((cur_k & a[i]) == 0) v1 += cur_k;
-			else v2 += cur_k;
+		if (value + zeros[iofk] <= m) {
+			k |= (1 << iofk);
+			if (get_value(a, iofk - 1, value + zeros[iofk])) return true;
 		}
-
-		if (v1 <= m) {
-			k |= cur_k;
-			if (get_value(a, iofk - 1, v1)) return true;
-		}
-		if (v2 <= m) {
-			k = (k >> (iofk + 1)) << iofk + 1;
-			if (get_value(a, iofk - 1, v2)) return true;
+		if (value + ones[iofk] <= m) {
+			k = (k >> (iofk + 1)) << (iofk + 1);
+			if (get_value(a, iofk - 1, value + ones[iofk])) return true;
 		}
 		return false;
 	}
@@ -44,51 +55,12 @@ public:
 		int len = get_size(a, max);
 		k = 0;
 		m = max;
+		init_elems(a, len + 1);
 		if (get_value(a, len, 0)) return k;
 		else return -1;
 	}
 };
 
-/*
-int get_max_k(int m, vector<int> &a) {
-    int k = 0, value = 0, temp_value1, temp_value2, 
-        cur_k, len = get_size(a, m), flag;
-
-    for (int i = len; i >= 0; --i) {
-        cur_k = 1 << i;
-        temp_value1 = value;
-        temp_value2 = value;
-		flag = 0;
-        for (int j = 0; j < a.size(); ++j) {
-            if ((cur_k & a[j]) == 0) temp_value1 += cur_k;
-			else {
-				temp_value2 += cur_k;
-				++flag;
-			}
-        }
-		if (flag <= a.size() - flag) {
-			if (temp_value1 <= m) {
-				k |= cur_k;
-				value = temp_value1;
-			} else if (temp_value2 <= m) {
-				value = temp_value2;
-			} else {
-				return -1;
-			}
-		} else {
-			if (temp_value2 <= m) {
-				value = temp_value2;
-			}else if (temp_value1 <= m) {
-				k |= cur_k;
-				value = temp_value1;
-			} else {
-				return -1;
-			}
-		}
-    }
-    return k;
-}
-*/
 
 int main(int argc, char **argv) {
 	Solution s;
